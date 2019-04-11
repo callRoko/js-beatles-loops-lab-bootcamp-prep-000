91cc2023610cbed46e83735c93a60bb0442421bf
Hazel Donaldson <hazel90.hd@gmail.com>
2019-04-11 15:51:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+// const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+// const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments){
   var counter = []
  for (let players = 0; players < musicians.length; players++){
